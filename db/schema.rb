4ae1f152c06c184f7f8ed4d0d79b9ff64b588872
User <user@Admins-MacBook-Pro.local>
2012-09-28 12:32:26 -0400

    committed persistant sessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,30 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120928144441) do
+ActiveRecord::Schema.define(:version => 20120928163045) do
 
   create_table "microposts", :force => true do |t|
-    t.string   "content"
-    t.integer  "user_id"
+    t.string    "content"
+    t.integer   "user_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+  end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "name"
+    t.string    "email"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
 end
